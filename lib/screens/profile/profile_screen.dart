12d075c89def0d52c41e12193b0bff4bb1a48f68
PieroLoa <piero.loarte32@gmail.com>
2026-09-11 15:25:16 -0500

    cambios 1

--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -23,7 +23,7 @@ class ProfileScreen extends StatefulWidget {
 
 class _ProfileScreenState extends State<ProfileScreen> {
   String _userName = 'Usuario';
-  String _userDescription = 'Amante de los cultivos';
+  String _userDescription = 'Amante de los cultivos xd';
   String? _profileImagePath;
 
   @override
